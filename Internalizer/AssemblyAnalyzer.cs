2e970ed7accc2190773f068f5eedaa19c2d5e930
Llewellyn Pritchard <xacc.ide@gmail.com>
2013-07-02 21:00:10 +0200

    Reorganize

---
+++ Internalizer/AssemblyAnalyzer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mono.Cecil;
+using MoreLinq;
+
+namespace Internalizer
+{
+  static class AssemblyAnalyzer
+  {
+    public static IEnumerable<MemberReference> AnalyzeMembers(AssemblyDefinition mainass, IEnumerable<AssemblyDefinition> depasses)
+    {
+      var musages = from dep in depasses
+                    from mref in dep.MainModule.GetMemberReferences()
+                    where ((AssemblyNameReference)mref.DeclaringType.Scope).FullName == mainass.Name.FullName
+                    let m = Scrutinize(mref)
+                    where m != null
+                    select m;
+
+      return musages.DistinctBy(x => x.FullName).OrderBy(x => x.Name);
+    }
+
+    public static IEnumerable<TypeReference> AnalyzeTypes(AssemblyDefinition mainass, IEnumerable<AssemblyDefinition> depasses)
+    {
+      var tusages = from dep in depasses
+                    from tref in dep.MainModule.GetTypeReferences()
+                    where ((AssemblyNameReference)tref.Scope).FullName == mainass.Name.FullName
+                    select tref;
+
+      return tusages.DistinctBy(x => x.FullName).OrderBy(x => x.FullName);
+    }
+
+    static MemberReference Scrutinize(MemberReference mref)
+    {
+      var gentype = mref.DeclaringType as GenericInstanceType;
+      if (gentype != null)
+      {
+        var methref = mref as MethodReference;
+        if (methref != null)
+        {
+          return methref.Resolve();
+        }
+      }
+      var genmeth = mref as MethodReference;
+      if (genmeth != null && genmeth.HasGenericParameters)
+      {
+        return genmeth.Resolve();
+      }
+      return mref;
+    }
+
+
+  }
+}
--- Internalizer/Program.cs
+++ Internalizer/Program.cs
@@ -2,52 +2,13 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Mono.Cecil;
+using MoreLinq;
 
 namespace Internalizer
 {
   class Program
   {
-    static void Main(string[] args)
-    {
-      var mainfile = args[0];
-
-      var rest = new List<string>(args.Skip(1).SelectMany(GetFilesFromArg).Except(new [] { mainfile }));
-
-      var mainass = AssemblyDefinition.ReadAssembly(mainfile);
-
-      var usages = from depfile in rest
-                   select AssemblyDefinition.ReadAssembly(depfile)
-                   into dep
-                   from mref in dep.MainModule.GetMemberReferences()
-                   where ((AssemblyNameReference) mref.DeclaringType.Scope).FullName == mainass.Name.FullName
-                   let m = Scrutinize(mref)
-                   where m != null
-                   select m.ToString();
-
-      foreach (var use in usages.Distinct().OrderBy(x => x))
-      {
-        Console.WriteLine(use);
-      }
-    }
-
-    static MemberReference Scrutinize(MemberReference mref)
-    {
-      var gentype = mref.DeclaringType as GenericInstanceType;
-      if (gentype != null)
-      {
-        var methref = mref as MethodReference;
-        if (methref != null)
-        {
-          var genref = gentype.Resolve().Methods.SingleOrDefault(x => x.Name == mref.Name && x.Parameters.Count == methref.Parameters.Count);
-          return genref;
-        }
-      }
-      return mref;
-    }
-
     static IEnumerable<string> GetFilesFromArg(string arg)
     {
       if (File.Exists(arg))
@@ -62,5 +23,23 @@ namespace Internalizer
         }
       }
     }
+
+    static void Main(string[] args)
+    {
+
+      var mainfile = args[0];
+
+      var rest = new List<string>(args.Skip(1).SelectMany(GetFilesFromArg).Except(new [] { mainfile }));
+
+      var mainass = AssemblyDefinition.ReadAssembly(mainfile);
+
+      var depasses = rest.Select(AssemblyDefinition.ReadAssembly).ToList();
+
+      AssemblyAnalyzer.AnalyzeTypes(mainass, depasses).ForEach(Console.WriteLine);
+      AssemblyAnalyzer.AnalyzeMembers(mainass, depasses).ForEach(Console.WriteLine);
+    }
+
+
+
   }
 }
